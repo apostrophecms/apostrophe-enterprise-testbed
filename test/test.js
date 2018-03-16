6d4cb6aea0e304083faa54a90608f584660df398
Tom Boutell <tom@punkave.com>
2018-03-16 10:22:56 -0400

    pass a custom argv to avoid a fight over command line

--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
 global.testing = true;
-var apos = require('../app.js')(function() {
+var apos = require('../app.js')({ argv: { _: {} } }, function() {
   console.log("Ready to run tests now.");
   // Run my tests then...
   console.log("Exiting after tests.");
-  process.exit(0);
+  apos.destroy(function(err) {
+    if (err) {
+      console.error('Problems destroying apostrophe.');
+    }
+    // TODO: this should exit cleanly on its own, but
+    // currently there is still some leak in the apos object,
+    // perhaps an interval or timeout?
+    process.exit(0);
+  });
 });
