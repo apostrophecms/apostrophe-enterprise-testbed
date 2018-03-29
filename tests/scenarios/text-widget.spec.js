09c9b2fde1bddb6845bb98d2cd36274bcfe50000
Sasha H <sasha.h@speedandfunction.com>
2018-03-29 09:24:13 -0400

    Implement "Text Widget" test

---
+++ tests/scenarios/text-widget.spec.js
@@ -0,0 +1,59 @@
+const _ = require('lodash');
+const server = require('../server');
+const steps = require('../steps');
+
+let contentMainBlockSelector;
+let richTextSelector;
+
+module.exports = Object.assign(
+  {
+    before: (client, done) => {
+      client.resizeWindow(1200, 800);
+
+      this._server = server.create();
+      this._server.start(done);
+
+      contentMainBlockSelector = '.demo-main';
+      richTextSelector = `${contentMainBlockSelector} [data-rich-text]`;
+    },
+
+    after: (client, done) => {
+      client.end();
+      this._server.stop(done);
+    },
+  },
+  steps.main(),
+  steps.login(),
+  steps.switchLocale('en'),
+  steps.switchToDraftMode(),
+  steps.makeSubPage('Regression test'),
+  {
+    'add Rich Text widget': function(client) {
+      const addContentBtnSelector = `${contentMainBlockSelector} [data-apos-add-content]`;
+      const richTextBtnSelector = `${contentMainBlockSelector} [data-apos-add-item=apostrophe-rich-text]`;
+
+      client.click(addContentBtnSelector);
+      client.click(addContentBtnSelector);
+      client.click(richTextBtnSelector);
+      client.pause(5000);
+      client.execute(function () {
+        const ckeditorInstance = _.find(CKEDITOR.instances);
+
+        ckeditorInstance.setData('Rich Text Widget line');
+      });
+
+      client.expect.element(richTextSelector).text.to.contain('Rich Text Widget line');
+    }
+  },
+  steps.submitChanges(),
+  steps.commitChanges(),
+  steps.switchToLiveMode(),
+  steps.navigateToHome(),
+  steps.confirm200ByRelativeUrl('regression-test'),
+  steps.navigateToPage('regression-test'),
+  {
+    'should have the Rich Text widget': function(client) {
+      client.expect.element(richTextSelector).text.to.contain('Rich Text Widget line');
+    }
+  },
+);
