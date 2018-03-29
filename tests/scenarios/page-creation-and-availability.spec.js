5fdc1c71c2088d52219f913eba303fffe4bcc851
Sasha H <sasha.h@speedandfunction.com>
2018-03-29 09:24:13 -0400

    Implement "Text Widget" test

--- tests/scenarios/page-creation-and-availability.spec.js
+++ tests/scenarios/page-creation-and-availability.spec.js
@@ -2,6 +2,19 @@ const server = require('../server');
 const steps = require('../steps');
 
 module.exports = Object.assign(
+  {
+    before: (client, done) => {
+      client.resizeWindow(1200, 800);
+
+      this._server = server.create();
+      this._server.start(done);
+    },
+
+    after: (client, done) => {
+      client.end();
+      this._server.stop(done);
+    },
+  },
   steps.main(),
   steps.login(),
   steps.switchLocale('en'),
@@ -17,16 +30,4 @@ module.exports = Object.assign(
   steps.navigateToHome(),
   steps.switchToLiveMode(),
   steps.confirm200ByRelativeUrl('regression-test'),
-{
-  before: (client, done) => {
-    client.resizeWindow(1200, 800);
-
-    this._server = server.create();
-    this._server.start(done);
-  },
-
-  after: (client, done) => {
-    client.end();
-    this._server.stop(done);
-  },
-});
+);
