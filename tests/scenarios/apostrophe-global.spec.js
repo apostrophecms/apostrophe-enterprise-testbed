326de145426c61ba746b5865d7faa3dbef937353
Paul Walker <starsinmypockets@gmail.com>
2018-05-10 18:31:15 -0400

    Minimal case for test foobar

--- tests/scenarios/apostrophe-global.spec.js
+++ tests/scenarios/apostrophe-global.spec.js
@@ -21,17 +21,4 @@ module.exports = Object.assign(
   steps.main(),
   steps.login(),
   steps.switchLocale('es'),
-  steps.switchToDraftMode(),
-  steps.makeSubPage('Regression test'),
-  steps.commitAndExport(),
-  steps.addTextWidgetTo({selector: '.footer', text: 'Rich Text Widget line global'}),
-  steps.commitAndExport(),
-  steps.switchLocale('en'),
-  {
-    'should have the Rich Text widget in the footer': function(client) {
-      const richTextSelector = `.footer [data-rich-text]`;
-
-      client.expect.element(richTextSelector).text.to.contain('Rich Text Widget line global');
-    }
-  },
 );
