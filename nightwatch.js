b6c43448dc769ad0db251f1becbbd6c8e87f959f
Paul Walker <starsinmypockets@gmail.com>
2018-05-10 19:52:33 -0400

    ugh looking for the right saucelibs tunnel config

--- nightwatch.js
+++ nightwatch.js
@@ -38,6 +38,7 @@ module.exports = {
       access_key: process.env.SAUCE_ACCESS_KEY,
       apos_address: 'localhost',
       apos_port: 3111,
+      'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
       silent: true,
       screenshots: {
         enabled: true,
