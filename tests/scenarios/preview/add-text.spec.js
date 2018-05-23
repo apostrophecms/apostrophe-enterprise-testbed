f6f1fe72c946cbe60213a423d26515cb82d3f1da
Paul Walker <starsinmypockets@gmail.com>
2018-05-22 20:05:48 -0400

    Update preview spec

--- tests/scenarios/preview/add-text.spec.js
+++ tests/scenarios/preview/add-text.spec.js
@@ -2,23 +2,25 @@ const cheerio = require('cheerio');
 const request = require('request');
 const server = require('../../server');
 const steps = require('../../steps/index');
+const sauce = require('../../sauce');
 
 module.exports = Object.assign(
   {
     before: (client, done) => {
       client.resizeWindow(1200, 800);
-
-      this._server = server.create();
-      this._server.start(done);
+      if (!this._server) {
+        this._server = server.create('localhost', 3111, 'app_2');
+        this._server.start(done);
+      }
     },
-
+    afterEach: sauce,
     after: (client, done) => {
       client.end(() => {
         this._server.stop(done);
       });
     },
   },
-  steps.main(),
+	steps.main(),
   steps.login(),
   steps.switchLocale('es'),
   steps.switchToDraftMode(),
