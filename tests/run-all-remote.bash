5a6311ea3c541fc35ee175b4312d4c6b520f09f0
Tom Boutell <tom@punkave.com>
2018-05-30 14:48:22 -0400

    break out scenarios to separate nightwatch invocations

---
+++ tests/run-all-remote.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Separate nightwatch invocations to stomp out disparities in behavior,
+# an experiment by Tom and Paul.
+
+for scenario in tests/scenarios/*; do
+  nightwatch $scenario --config nightwatch.js --env remote || exit 1
+  # Let sockets go away? Sleeping dogs lie? Something?
+  sleep 5
+done
+
