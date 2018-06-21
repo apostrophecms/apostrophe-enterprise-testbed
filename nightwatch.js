4aa59c801b028d90e6792f7266570b6cdbbce301
Tom Boutell <tom@punkave.com>
2018-06-21 15:54:57 -0400

    maybe fullscreen is not a thing on linux

--- nightwatch.js
+++ nightwatch.js
@@ -25,7 +25,7 @@ module.exports = {
         javascriptEnabled: true,
         acceptSslCerts: true,
         "chromeOptions": {
-          "args": [ "start-fullscreen" ]
+          "args": [ "start-maximized" ]
         }
       }
     },
