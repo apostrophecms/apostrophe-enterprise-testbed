e044bc2496befa29ea577e67deebe041ba8bed84
Tom Boutell <tom@punkave.com>
2018-06-22 11:31:12 -0400

    verbose logging

--- tests/run-all-local.bash
+++ tests/run-all-local.bash
@@ -3,7 +3,7 @@
 # Separate nightwatch invocations to stomp out disparities in behavior
 
 for scenario in tests/scenarios/*; do
-  npx nightwatch $scenario --config nightwatch.js --env local || exit 1
+  npx nightwatch $scenario --config nightwatch.js --env local --verbose || exit 1
   # Let sockets go away? Sleeping dogs lie? Something?
   sleep 5
 done
