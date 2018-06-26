aae75509263db8d636c836472adff292257bc379
Tom Boutell <tom@punkave.com>
2018-06-26 16:07:30 -0400

    remote is remote

--- tests/run-all-remote.bash
+++ tests/run-all-remote.bash
@@ -4,14 +4,14 @@
 
 mkdir -p screenshots/latest
 
-npx nightwatch tests/scenarios/* --config nightwatch.js --env local --verbose &&
+npx nightwatch tests/scenarios/* --config nightwatch.js --env remote --verbose &&
 
 node visual-regression-test.js &&
 
 # Separate nightwatch invocations to stomp out disparities in behavior
 
 # for scenario in tests/scenarios/*; do
-#   npx nightwatch $scenario --config nightwatch.js --env local --verbose || exit 1
+#   npx nightwatch $scenario --config nightwatch.js --env remot --verbose || exit 1
 #   # Let sockets go away? Sleeping dogs lie? Something?
 #   sleep 5
 # done
