243a8b3e2c1515f08e148234d28b9c5fe7f3836b
Thomas Boutell <tom@punkave.com>
2019-06-13 11:16:19 -0400

    correct package

--- ci-helpers/install-mongo-cli.bash
+++ ci-helpers/install-mongo-cli.bash
@@ -5,5 +5,5 @@ apt-get install -y apt-transport-https &&
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 &&
 echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list &&
 apt-get update &&
-apt-get install -y mongodb-org-shell &&
+apt-get install -y mongodb-org-shell mongodb-org-tools &&
 mongo --version && echo ">>> SUCCESSFUL MONGO SHELL INSTALL"
