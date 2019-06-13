27ec1fcf211dff1a9f037aec4d285320b6165ecd
Thomas Boutell <tom@punkave.com>
2019-06-13 11:27:21 -0400

    need lsof

--- ci-helpers/install-mongo-cli.bash
+++ ci-helpers/install-mongo-cli.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 lsb_release -a &&
-apt-get install -y apt-transport-https &&
+apt-get install -y lsof apt-transport-https &&
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 &&
 echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list &&
 apt-get update &&
