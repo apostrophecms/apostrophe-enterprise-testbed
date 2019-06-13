4ab416627afcc34f487d705f0e7fc3ff04a7f3dd
Thomas Boutell <tom@punkave.com>
2019-06-13 10:28:45 -0400

    debian stretch

--- ci-helpers/install-mongo-cli.bash
+++ ci-helpers/install-mongo-cli.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "**** VERSION OF DEBIAN:"
-lsb_release -a
-echo "^^^^ VERSION UP HERE"
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-apt-get update
-apt-get install -y mongodb-org-shell
+lsb_release -a &&
+apt-get install -y apt-transport-https &&
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4 &&
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list &&
+apt-get update &&
+apt-get install -y mongodb-org-shell &&
 mongo --version && echo ">>> SUCCESSFUL MONGO SHELL INSTALL"
