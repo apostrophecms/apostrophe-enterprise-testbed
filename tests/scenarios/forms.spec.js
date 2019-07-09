20aa50a1e1de3b0d1a85635c052a79e65fde3c28
Alex Bea <alex.bea@gmail.com>
2019-07-09 11:23:20 -0500

    Skips form creation with a task

--- tests/scenarios/forms.spec.js
+++ tests/scenarios/forms.spec.js
@@ -14,7 +14,7 @@ module.exports = Object.assign(
           console.error(err);
           return done(err);
         }
-
+        this._server.task('apostrophe-forms:addForm');
         // Migration task must not crash when run noninteractively (pipe to file)
         if (this._server.task('apostrophe-migrations:migrate > /tmp/migrate.results').code !== 0) {
           return done('MIGRATION ERROR');
@@ -35,15 +35,12 @@ module.exports = Object.assign(
   steps.switchToDraftMode(),
   steps.makeSubPage('Form test'),
   {
-    'create a form': (client) => {
+    'Add a form to a page': (client) => {
       const mainBlockSelector = '.demo-main';
       const addContentBtnSelector = `${mainBlockSelector} [data-apos-add-content]`;
       const formWidgetBtnSelector = `${mainBlockSelector} [data-apos-add-item=apostrophe-forms]`;
       const browseBtnSelector = '[data-apos-browse]';
-      const newFormBtnSelector = '[data-apos-create-apostrophe-forms]';
-      const formEditorModal = '[data-apos-modal-current="apostrophe-forms-editor-modal"]';
-      const formTabSelector = '[data-apos-open-group="form"]';
-      const addFieldSelector = `[data-name="contents"]  [data-apos-add-content]`;
+
       const saveWidgetBtnSelector = '[data-apos-save]';
 
       // To make sure we scroll down far enough to eliminate certain conflicts
@@ -53,27 +50,7 @@ module.exports = Object.assign(
       client.waitForModal('apostrophe-forms-widgets-editor');
       client.clickInModal('apostrophe-forms-widgets-editor', browseBtnSelector);
       client.waitForModal('apostrophe-forms-manager-modal');
-      client.clickInModal('apostrophe-forms-manager-modal', newFormBtnSelector);
-      client.waitForModal('apostrophe-forms-editor-modal');
-      client.setValue(`${formEditorModal} input[name="title"]`, 'Contact Form');
-      client.setValue(`${formEditorModal} input[name="slug"]`, 'contact-form');
-      client.clickInModal('apostrophe-forms-editor-modal', formTabSelector);
-
-      // Add a required text field
-      client.clickInModal('apostrophe-forms-editor-modal', addFieldSelector);
-      client.clickInModal('apostrophe-forms-editor-modal', '[data-apos-add-item=apostrophe-forms-text-field]');
-      client.waitForModal('apostrophe-forms-text-field-widgets-editor');
-      client.setValue(`input[name="fieldLabel"]`, 'Your Name');
-      client.setValue(`input[name="fieldName"]`, 'name');
-      client.setValue(`select[name="required"]`, '1');
-      client.clickInModal('apostrophe-forms-text-field-widgets-editor', saveWidgetBtnSelector);
-      client.waitForModal('apostrophe-forms-editor-modal');
-
-      client.setValue('[name="submitLabel"]', 'Send');
-      // Save form.
-      client.pause(5000);
-      client.clickInModal('apostrophe-forms-editor-modal', saveWidgetBtnSelector);
-      client.waitForModal('apostrophe-forms-manager-modal');
+      client.clickInModal('apostrophe-forms-manager-modal', '[data-apos-manage-view] [data-piece]:first-child label');
       client.clickInModal('apostrophe-forms-manager-modal', saveWidgetBtnSelector);
       client.waitForModal('apostrophe-forms-widgets-editor');
       client.clickInModal('apostrophe-forms-widgets-editor', saveWidgetBtnSelector);
